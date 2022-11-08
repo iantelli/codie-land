dff845bada21ae59f1e5fefadeabfc91f501d9d8
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-11-08 15:58:53 -0800

    script component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
           href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/hybrid.min.css"
         />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
+        <Script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></Script>
       </Head>
       <body>
         <Main />
